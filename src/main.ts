8558d148a10b98f65103378c86155fb5628738db
AndDev <23031429@itcelaya.edu.mx>
2025-05-26 17:12:03 -0600

    feat: initialize Three.js project with basic setup
    - Added main.ts to set up a basic Three.js scene with a red wireframe cube.
    - Created style.css for basic styling of the canvas and body.
    - Included vite-env.d.ts for Vite client types.
    - Configured TypeScript with tsconfig.json for ES2020 and strict mode.
    - Added vite.svg logo to the public directory.

---
+++ src/main.ts
@@ -0,0 +1,30 @@
+import "./style.css";
+import * as THREE from "three";
+
+const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+
+const [width, height] = [canvas.width, canvas.height];
+
+// scene
+const scene = new THREE.Scene();
+
+// object
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({
+  color: 0xff0000,
+  wireframe: true,
+});
+
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+// camera
+const camera = new THREE.PerspectiveCamera(75, width / height);
+camera.position.z = 5;
+scene.add(camera);
+
+const renderer = new THREE.WebGLRenderer({ canvas });
+
+renderer.setSize(width, height);
+
+renderer.render(scene, camera);
